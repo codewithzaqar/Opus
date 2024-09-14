74394cf47a2dbcac0d081a1977341ea6355646cd
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-09-14 18:07:10 +0400

    editing keybinds

--- js/quill.js
+++ js/quill.js
@@ -1,57 +1,165 @@
-var toolbarOptions = [
-  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-  ['blockquote', 'code-block'],
-
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
-
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
-
-  ['clean']                                         // remove formatting button
-];
-
 var quill = new Quill('.editor', {
-
-  modules: {
-    toolbar: toolbarOptions
-  },
-
   debug: 'info',
-  placeholder: 'Begin your opus...',
   theme: 'snow'
 });
+
+
+/* Strikethrough */
+quill.keyboard.addBinding({
+  key: 'S',
+  shiftKey: true,
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('strike', !context.format['strike']);
+  }
+});
+
+/* Heading 1 */
+quill.keyboard.addBinding({
+  key: '1',
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('header', '1');
+  }
+});
+
+/* Heading 2 */
+quill.keyboard.addBinding({
+  key: '2',
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('header', '2');
+  }
+});
+
+/* Heading 3 */
+quill.keyboard.addBinding({
+  key: '3',
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('header', '3');
+  }
+});
+
+/* Clear formatting */
+quill.keyboard.addBinding({
+  key: '0',
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.removeFormat(this.quill.getSelection());
+  }
+});
+
+/* Ordered list */
+quill.keyboard.addBinding({
+  key: 'L',
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('list', 'ordered');
+  }
+});
+
+/* Unordered list */
+quill.keyboard.addBinding({
+  key: 'L',
+  shiftKey: true,
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('list', 'bullet');
+  }
+});
+
+/* Subscript */
+quill.keyboard.addBinding({
+  key: 189, // -
+  shiftKey: true,
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('script', 'sub', true);
+  }
+});
+
+/* Superscript */
+quill.keyboard.addBinding({
+  key: 187, // =
+  shiftKey: true,
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('script', 'super', true);
+  }
+});
+
+/* Code-block */
+quill.keyboard.addBinding({
+  key: "C",
+  shiftKey: true,
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('code-block', !context.format['code-block']);
+  }
+});
+
+/* Blockquote */
+quill.keyboard.addBinding({
+  key: 190, // .
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('blockquote', !context.format['blockquote']);
+  }
+});
+
+/* Indent */
+quill.keyboard.addBinding({
+  key: 221, // ]
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('indent', '+1');
+  }
+});
+
+/* Outdent */
+quill.keyboard.addBinding({
+  key: 219, // ]
+  shortKey: true,
+  handler: function(range, context) {
+    this.quill.format('indent', '-1');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 var Delta = Quill.import('delta');
 
 function loadFile() {
   // Load Delta content from file
-  var contents = fs.readFileSync('/Users/paco/Dropbox/school/opus/new').toString()
+  var contents = fs.readFileSync('/Users/paco/Dropbox/school/opus/new').toString();
 
-  var load = new Delta(JSON.parse(contents))
+  var load = new Delta(JSON.parse(contents));
 
-  quill.setContents(load, "user")
+  quill.setContents(load, "user");
 }
 
 // Set a flag on text-change event.
-var change = false
+var change = false;
 quill.on('text-change', function(delta) {
-  change = true
+  change = true;
 });
 
 // Every second, monitor the text-change flag
 // If true, save the entire file locally.
 setInterval(function() {
-  if (change) {
-    console.log('Saving file.');
+  if(change) {
+    console.log("Saving file.");
 
     writeFile(JSON.stringify(quill.getContents()));
 
--- main.js
+++ main.js
@@ -8,47 +8,67 @@ const settings = require('electron-settings')
 let win
 
 function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({
-        width: 960,
-        height: 544,
-        frame: false,
-        webPreferences: {
-            experimentalFeatures: true
-        }
-    })
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 960,
+    height: 544,
+    frame: false,
+    webPreferences: {
+      experimentalFeatures: true
+    }
+  })
 
-    settings.set('editor', {
-        path: '/Users/paco/Dropbox/school/opus/'
-    })
+  let webContents = win.webContents;
+  webContents.on('did-finish-load', () => {
+    webContents.setZoomFactor(1);
+    webContents.setVisualZoomLevelLimits(1, 1);
+    webContents.setLayoutZoomLevelLimits(0, 0);
+  });
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+  settings.set('editor', {
+    path: '/Users/paco/Dropbox/school/opus/'
+  })
 
-    // Open the DevTools.
-    //win.webContents.openDevTools()
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-    //Emitted when the window is closed.
-    win.on('closed', () => {
-        win = null
-    })
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
 }
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-//Quit when all windows are closed.
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin') {
-        app.quit()
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow()
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
 })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
