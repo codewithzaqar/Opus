e2a2bd5d7ecb44428236cb4655d72b9b81c536c4
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-09-14 17:25:43 +0400

    load/store progress

--- js/quill.js
+++ js/quill.js
@@ -1,33 +1,65 @@
 var toolbarOptions = [
-    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-    ['blockquote', 'code-block'],
-  
-    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-    [{ 'direction': 'rtl' }],                         // text direction
-  
-    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
-    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-    [{ 'font': [] }],
-    [{ 'align': [] }],
-  
-    [ 'formula' ],
-  
-    ['clean']                                         // remove formatting button
-  ];
-  
-  var quill = new Quill('.editor', {
-  
-    modules: {
-      formula: true,
-      toolbar: toolbarOptions
-    },
-  
-    debug: 'info',
-    placeholder: 'Begin your opus...',
-    theme: 'snow'
-  });
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
+var quill = new Quill('.editor', {
+
+  modules: {
+    toolbar: toolbarOptions
+  },
+
+  debug: 'info',
+  placeholder: 'Begin your opus...',
+  theme: 'snow'
+});
+
+
+/* Load delta contents */
+var Delta = Quill.import('delta');
+
+var contents = fs.readFileSync('/Users/paco/Dropbox/school/opus/new').toString();
+
+var load = new Delta(JSON.parse(contents));
+
+quill.setContents(load, "user");
+
+
+// Store accumulated changes
+var change = new Delta();
+quill.on('text-change', function(delta) {
+  change = change.compose(delta);
+});
+
+// Save periodically
+setInterval(function() {
+  if (change.length() > 0) {
+    console.log('Saving changes', change);
+
+    writeFile(JSON.stringify(change));
+
+    change = new Delta();
+  }
+}, 5*1000);
+
+// Check for unsaved data
+window.onbeforeunload = function() {
+  if (change.length() > 0) {
+    return 'There are unsaved changes. Are you sure you want to leave?';
+  }
+}
